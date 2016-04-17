87a82188743ab42245b6e64bc08d6f0aa8c60633
Said Karimov <saidk@machinecdn.com>
2016-04-17 22:51:56 +0500

    first

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-app.get('/facebook', function (req, res) {
+router.get('/facebook', function (req, res) {
   if (req.query['hub.verify_token'] === '123321') {
     res.send(req.query['hub.challenge']);
   } else {
